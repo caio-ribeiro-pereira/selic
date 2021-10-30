82618a2c26bbd616238db699d62486dffac4f901
Michael Wu <michael.wu@meuacerto.com.br>
2021-10-30 15:35:55 -0300

    add description for methods

--- src/selic.ts
+++ src/selic.ts
@@ -18,7 +18,12 @@ export class Selic {
     this.poupancaPercent = poupancaPercent;
   }
 
-  async scrapRates() {
+  /**
+  * Fetch and calculate the brazilian selic, poupanca and cdi rates apy
+  *
+  * @returns {Array} list with selic, poupanca and cdi rates apy
+  */
+  async scrapRates(): Promise<Rate[]> {
     const selic = await this.getSelicRate();
     const cdi = this.calculateCdiFromSelic(selic);
     const poupanca = this.calculatePoupancaFromSelic(selic);
@@ -29,27 +34,54 @@ export class Selic {
     ];
   }
 
+  /**
+  * Fetch and calculate brazilian selic rates apy
+  *
+  * @returns {Number} fetched selic rates apy
+  */
   async getSelicRate() {
     const selic = await fetchCurrentSelic();
     return Number(Number(selic).toFixed(2));
   }
 
+  /**
+  * Fetch and calculate cdi rate from selic value
+  *
+  * @returns {Number} fetched cdi rates apy
+  */
   async getCdiRate() {
     const selic = await this.getSelicRate();
     return this.calculateCdiFromSelic(selic);
   }
 
+  /**
+  * Fetch and calculate poupanca rate from selic value
+  *
+  * @returns {Number} fetched poupanca rates apy
+  */
   async getPoupancaRate() {
     const selic = await this.getSelicRate();
     return this.calculatePoupancaFromSelic(selic);
   }
 
-  calculateCdiFromSelic(selic: number) {
+  /**
+  * Calculate the cdi rate from selic value
+  *
+  * @param {number} selic selic rate apy for calculation
+  * @returns {Number} cdi rates apy
+  */
+  calculateCdiFromSelic(selic: number): number {
     const cdi = selic - this.cdiScore;
     return Number(Number(cdi).toFixed(2));
   }
 
-  calculatePoupancaFromSelic(selic: number) {
+  /**
+  * Calculate the poupanca rate from selic value
+  *
+  * @param {number} selic selic rate apy for calculation
+  * @returns {Number} poupanca rates apy
+  */
+  calculatePoupancaFromSelic(selic: number): number {
     const poupanca = (selic / 100) * this.poupancaPercent;
     return Number(Number(poupanca).toFixed(2));
   }
