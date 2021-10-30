bd242072b5ca11ea0c05a7faca562ba0a6bf545d
Michael Wu <michael.wu@meuacerto.com.br>
2021-10-30 14:56:38 -0300

    remove default selic value

--- src/selic.ts
+++ src/selic.ts
@@ -39,12 +39,12 @@ export class Selic {
     return this.calculatePoupancaFromSelic(selic);
   }
 
-  calculateCdiFromSelic(selic: number = 0) {
+  calculateCdiFromSelic(selic: number) {
     const cdi = selic - this.cdiScore;
     return Number(Number(cdi).toFixed(2));
   }
 
-  calculatePoupancaFromSelic(selic: number = 0) {
+  calculatePoupancaFromSelic(selic: number) {
     const poupanca = (selic / 100) * this.poupancaPercent;
     return Number(Number(poupanca).toFixed(2));
   }
