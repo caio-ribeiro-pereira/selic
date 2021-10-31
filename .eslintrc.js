5e34a1cf05a8412bdf20b1ea0885ceca8f412adb
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2021-10-31 15:58:06 -0300

    fix tsconfig for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
